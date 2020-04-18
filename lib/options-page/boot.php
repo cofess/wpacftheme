33bd4e2e95c2b30e2b834b6dc872a4becdad0a68
cofess <285088180@qq.com>
2020-04-18 17:37:53 +0800

    no message

--- lib/options-page/boot.php
+++ lib/options-page/boot.php
@@ -12,7 +12,7 @@ function acf_options_init() {
     'page_title' => 'Site Options',
     'menu_title' => 'Site Options',
     'menu_slug' => 'common_site_options',
-    'capability' => 'edit_posts',
+    'capability' => 'manage_options',
     'update_button'   => __('Update', 'acf'),
     'updated_message' => __("Options Updated", 'acf'),
     // 'redirect' => true
@@ -20,17 +20,29 @@ function acf_options_init() {
   acf_add_options_sub_page([
     'page_title' => 'Contact Information',
     'menu_title' => 'Contact Info',
+    // 'menu_slug' => 'contact_info',
     'parent_slug' => 'common_site_options',
+    'capability' => 'manage_options',
+    'update_button'   => __('Update', 'acf'),
+    'updated_message' => __("Options Updated", 'acf'),
   ]);
   acf_add_options_sub_page([
     'page_title' => 'Code Injection',
     'menu_title' => 'Code Injection',
+    // 'menu_slug' => 'code_injection',
     'parent_slug' => 'common_site_options',
+    'capability' => 'manage_options',
+    'update_button'   => __('Update', 'acf'),
+    'updated_message' => __("Options Updated", 'acf'),
   ]);
   acf_add_options_sub_page([
     'page_title' => 'Other Services',
     'menu_title' => 'Other Services',
+    // 'menu_slug' => 'other_services',
     'parent_slug' => 'common_site_options',
+    'capability' => 'manage_options',
+    'update_button'   => __('Update', 'acf'),
+    'updated_message' => __("Options Updated", 'acf'),
   ]);
 
   // add_filter('acf/settings/save_json', 'my_acf_json_save_point');
--- lib/options-page/theme.options.php
+++ lib/options-page/theme.options.php
@@ -5,12 +5,95 @@ function my_acf_add_local_field_groups() {
     return;
   } 
 
-  $options = array();
-  require '_theme/base.fields.php';
+  // $options = array();
+  // require '_theme/base.fields.php';
 
-  acf_add_local_field_group(array('key' => 'group_cso_contact_info',
+  acf_add_local_field_group(array(
+    'key' => 'group_5e6ec344a808e',
+    'title' => 'Product image',
+    'menu_order' => 5,
+    'position' => 'side',
+    'style' => 'default',
+    'label_placement' => 'top',
+    'instruction_placement' => 'label',
+    'hide_on_screen' => '',
+    'active' => true,
+    'description' => '',
+    'fields' => array(
+        array(
+            'key' => 'field_5e6ec36ee4bbc',
+            'label' => 'Product image',
+            'name' => 'product_image',
+            'type' => 'image',
+            'instructions' => '',
+            'required' => 0,
+            'conditional_logic' => 0,
+            'wrapper' => array(
+                'width' => '',
+                'class' => '',
+                'id' => '',
+            ),
+            'return_format' => 'id',
+            'preview_size' => 'full',
+            'library' => 'all',
+            'min_width' => '',
+            'min_height' => '',
+            'min_size' => '',
+            'max_width' => '',
+            'max_height' => '',
+            'max_size' => '',
+            'mime_types' => '',
+        ),
+    ),
+    'location' => array (
+        array (
+          array ('param' => 'options_page',
+            'operator' => '==',
+            'value' => 'acf-options-contact-info',
+            ),
+          ),
+        ),
+));
+
+  acf_add_local_field_group(array('key' => 'group_contact_info',
       'title' => 'Contact Info',
-      'fields' => $options,
+      'menu_order' => 0,
+      'position' => 'normal',
+      // 'style' => 'seamless',
+      'label_placement' => 'top',
+      'instruction_placement' => 'label',
+      'hide_on_screen' => '',
+      'active' => true,
+      'description' => '',
+      'fields' => array (
+        array (
+            'key' => 'field_5bead261dabb4',
+            'label' => '基础设置',
+            'name' => '',
+            'type' => 'tab',
+            'instructions' => '',
+            'required' =>  0,
+            'conditional_logic' =>  0,
+            'placement' =>  'top',
+            "endpoint" =>  0
+        ),
+        array ('key' => 'field_1',
+          'label' => 'Sub Title2',
+          'name' => 'sub_title',
+          'type' => 'text',
+          ),
+        array (
+            'key' => 'field_2',
+            'label' => '优化设置',
+            'name' => '',
+            'type' => 'tab',
+            'instructions' => '',
+            'required' =>  0,
+            'conditional_logic' =>  0,
+            'placement' =>  'top',
+            "endpoint" =>  0
+        ),
+        ),
       'location' => array (
         array (
           array ('param' => 'options_page',
